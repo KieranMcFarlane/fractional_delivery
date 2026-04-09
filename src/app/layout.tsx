d51cfa59421c6871e3016af9414dcde6b9ee05ed
Kieran McFarlane <kieranmcfarlane2@googlemail.com>
2026-04-09 18:10:38 +0100

    fix: remove clarity from global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,18 +89,6 @@ gtag('js', new Date());
 gtag('config', '${googleTagId}');`,
           }}
         />
-        <Script
-          id="clarity"
-          strategy="beforeInteractive"
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `(function(c,l,a,r,i,t,y){
-  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "w49oisyyx4");`,
-          }}
-        />
       </head>
       <body className="min-h-full flex flex-col">
         <TooltipProvider>{children}</TooltipProvider>
